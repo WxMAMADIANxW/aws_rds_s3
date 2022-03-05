b70aa58e64aadbe69cdc5f000753be041110354f
WxMAMADIANxW <49515789+WxMAMADIANxW@users.noreply.github.com>
2022-03-05 03:27:10 +0100

    front

---
+++ front/run.py
@@ -0,0 +1,16 @@
+from flask import (Flask, request, render_template)
+import mysql.connector
+from decimal import Decimal
+import json
+import boto3
+from boto3.dynamodb.conditions import Key
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=3000)
