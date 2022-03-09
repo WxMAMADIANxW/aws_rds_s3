fcb33118d17337b882f6a6c203ae954db2941423
WxMAMADIANxW <49515789+WxMAMADIANxW@users.noreply.github.com>
2022-03-09 19:55:37 +0100

    test redirect

--- front/run.py
+++ front/run.py
@@ -1,10 +1,11 @@
-from flask import (Flask, request, render_template)
+from flask import (Flask, request, render_template, redirect)
 import mysql.connector
 from decimal import Decimal
 import json
 import boto3
 from boto3.dynamodb.conditions import Key
 from requests_html import HTMLSession
+
 
 app = Flask(__name__)
 
@@ -16,16 +17,16 @@ def welcome():
 
 @app.route("/s3_to_rds", methods=['GET','POST'])
 def s3_to_rds():
-    session = HTMLSession()
-    response = session.get("http://13.38.117.219:3000/s3_to_rds")
-    return render_template(response.html)
+    #session = HTMLSession()
+    #response = session.get("http://13.38.117.219:3000/s3_to_rds")
+    return redirect("http://13.38.117.219:3000/s3_to_rds")
 
 
 @app.route("/rds_to_s3", methods=['GET','POST'])
 def rds_to_s3():
-    session = HTMLSession()
-    response = session.get("http://13.38.117.219:3000/rds_to_s3")
-    return render_template(response.html)
+    #session = HTMLSession()
+    #response = session.get("http://13.38.117.219:3000/rds_to_s3")
+    return redirect("http://13.38.117.219:3000/rds_to_s3")
 
 
 if __name__ == "__main__":
